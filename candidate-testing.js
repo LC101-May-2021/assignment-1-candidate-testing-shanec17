dbba030b7df1d60c9180d81ede35a70258c2cd5e
shanec17 <84325994+shanec17@users.noreply.github.com>
2021-05-27 08:48:42 -0500

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,16 +14,9 @@ console.log; ("Hello"+ candidateName)
 
 
 
-
-
-
-
-const input = require('readline-sync');
-
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName;
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
